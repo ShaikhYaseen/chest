fbee1563d407c28e77e9463b76a2fe24f8c974ab
ShaikhYaseen <59789653+ShaikhYaseen@users.noreply.github.com>
2020-01-12 14:07:19 +0530

    Add files via upload
    A cpp program to print a sine wave in command prompt.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+double pi = 3.14159265359;
+int main() {
+    int alt = 30; //altitude. max 39
+    int length = 200; // length of sin wave
+    float dis= 0.3; //distance between two dots. 0.3 works fine
+
+    float y;
+    float deg=0;
+    for(int i=0 ; i<length ; i++){
+        y=sin(deg);
+        for(int j=0 ; j<alt+(y*alt) ; j++)
+            cout << " ";
+        cout << "*";
+        cout << endl;
+        deg = deg + dis;
+    }
+}
+
